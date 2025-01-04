40fcddd23c2c665fb94a87cf8efd8108a5ce4aa9
Forest Deng <forest.jinying.denghk@outlook.com>
2025-01-04 18:36:05 +0000

    fix 12

--- functions/[[path]].py
+++ functions/[[path]].py
@@ -1,17 +1,19 @@
 from flask import Flask, Request
 from app import app as flask_app
 
-def handler(request):
+def onRequest(context):
     """
     Entry point for Cloudflare Pages Functions
     """
     try:
+        request = context.get('request', {})
+        
         # Create Flask request
         flask_request = Request.from_values(
-            base_url=request.url,
-            method=request.method,
-            headers=dict(request.headers),
-            input_stream=request.body.encode() if request.body else None
+            base_url=request.get('url', ''),
+            method=request.get('method', 'GET'),
+            headers=dict(request.get('headers', {})),
+            input_stream=request.get('body', '').encode() if request.get('body') else None
         )
 
         # Process the request through Flask
